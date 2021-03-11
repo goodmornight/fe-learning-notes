2b7200f9c4c413e352761a0ee3825d83b89b79a7
GoodNight <goodnightlilanhui@gmail.com>
2021-03-12 00:17:34 +0800

    feat: list

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'GoodNight FE Notes',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -34,16 +34,44 @@ module.exports = {
     lastUpdated: false,
     nav: [
       {
-        text: 'Guide',
-        link: '/guide/',
+        text: '数据结构与算法',
+        link: '/algorithm/'
       },
       {
-        text: 'Config',
-        link: '/config/'
+        text: 'HTML+CSS',
+        link: '/algorithm/'
       },
       {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
+        text: 'JS',
+        link: '/algorithm/'
+      },
+      {
+        text: 'Vue',
+        link: '/algorithm/'
+      },
+      {
+        text: 'React',
+        link: '/algorithm/'
+      },
+      {
+        text: 'Webpack',
+        link: '/algorithm/'
+      },
+      {
+        text: 'NodeJS',
+        link: '/algorithm/'
+      },
+      {
+        text: '性能优化',
+        link: '/algorithm/'
+      },
+      {
+        text: '设计模式',
+        link: '/algorithm/'
+      },
+      {
+        text: '博客',
+        link: 'http://www.goodnight.wiki/'
       }
     ],
     sidebar: {
